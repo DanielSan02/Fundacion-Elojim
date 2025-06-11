8ca809b4b3b0f8874d2d03a03b5eabcfdf4d76b8
SebiceC <sebartin9@gmail.com>
2025-06-11 16:49:07 -0500

    correccion de errores en fuentes y despliegue

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,20 +1,10 @@
 "use client";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "@/components/ui/toaster";
 
 // Cargar las fuentes
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 const inter = Inter({
   subsets: ["latin"],
 });
@@ -23,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es" className="h-full bg-gray-100">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${inter.className} h-full`}>
+        className={`${inter.className} h-full`}>
         <SessionProvider>
           {children}
           <Toaster />
